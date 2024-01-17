f41ac49697cf7b64985e9ac895e9851b31a604df
Piti Ongmongkolkul <piti118@gmail.com>
2024-01-18 01:59:30 +0700

    Create exercise_05_typing.py

---
+++ src/pytest_tutorial/refactor_exercise/exercise_05_typing.py
@@ -0,0 +1,11 @@
+# Eventhough python is a duck type language. Add typing information typically save you a lot of headache.
+
+# check if the list containt at least n element of the specified string
+def has_at_least_n_char(a, b, s):
+  c = 0
+  for x in a:
+    if x == s
+      c += 1
+  return c >= b
+
+# refactor this so someone who needs to call this function doesn't come back and haut you.
